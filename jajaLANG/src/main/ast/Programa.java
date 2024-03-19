2c47d68c5b0f22a7f6357009fae262acd1e72e22
Mario <mcalvarr@ucm.es>
2024-03-19 18:22:24 +0100

    Added public

--- jajaLANG/src/main/ast/Programa.java
+++ jajaLANG/src/main/ast/Programa.java
@@ -7,7 +7,7 @@ import main.ast.declaraciones.ListaDeclaraciones;
 public class Programa {
     private List<Import> lista_imports;
     private ListaDeclaraciones lista_declaraciones;
-    Programa(List<Import> lista_imports, ListaDeclaraciones lista_declaraciones) {
+    public Programa(List<Import> lista_imports, ListaDeclaraciones lista_declaraciones) {
         this.lista_imports = lista_imports;
         this.lista_declaraciones = lista_declaraciones;
     }
