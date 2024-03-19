af366ed9fc474af3d865e11f946b10c6e214da5e
beñat P. de Arenaza <benperez@ucm.es>
2024-03-19 12:59:49 +0100

    OperadorBin

--- jajaLANG/src/main/ast/expresiones/Llamada.java
+++ jajaLANG/src/main/ast/expresiones/Llamada.java
@@ -2,6 +2,8 @@ package main.ast.expresiones;
 
 import java.util.List;
 
+import main.ast.declaraciones.DeclaracionVar;
+
 public class Llamada extends Expresion{
 
 	private List<Expresion> listaExpresiones;
@@ -13,8 +15,17 @@ public class Llamada extends Expresion{
 		this.listaExpresiones = listaExpresiones;
 		
 	}
-	
-
-	
-
+	public String toString() {
+		 StringBuilder sb = new StringBuilder();
+	        sb.append(exp).append("(");
+	        int i = 0;
+	        for (Expresion exp : listaExpresiones) {
+	        	sb.append(exp);
+	        	if(i+1!=listaExpresiones.size())
+	        		sb.append(",");
+	        	i++;
+	        }
+	        sb.append(")");  
+	        return sb.toString();
+	} 
 }
--- jajaLANG/src/main/ast/expresiones/OperadorBin.java
+++ jajaLANG/src/main/ast/expresiones/OperadorBin.java
@@ -2,7 +2,89 @@ package main.ast.expresiones;
 
 public class OperadorBin {
 
-	public OperadorBin(Operador op, Expresion exp1, Expresion exp2) {
-		
-	}
+	public class OperBin extends Expresion {
+	    public enum Operadores {
+	        SUMA,
+	        RESTA,
+	        MUL,
+	        DIV,
+	        MOD,
+	        IGUAL,
+	        DESIGUAL,
+	        MENOR,
+	        MAYOR,
+	        MANORIGUAL,
+	        MAYORIGUAL,
+	        DISY,
+	        CONJ,
+	        POT,
+	        NEG,
+	    	DIRECCION,
+	    	PUNTERO;
+
+	        public String toString() {
+	            switch (this) {
+	                case SUMA:
+	                    return "+";
+	                case RESTA:
+	                    return "-";
+	                case MUL:
+	                    return "*";
+	                case DIV:
+	                    return "/";
+	                case MOD:
+	                    return "%";
+	                case IGUAL:
+	                    return "==";
+	                case DESIGUAL:
+	                    return "!=";
+	                case MENOR:
+	                    return "<";
+	                case MAYOR:
+	                    return ">";
+	                case MANORIGUAL:
+	                    return "<=";
+	                case MAYORIGUAL:
+	                    return ">=";
+	                case CONJ:
+	                    return "&&";
+	                case DISY:
+	                    return "||";
+	                case POT:
+	                    return "^";
+	                case DIRECCION:
+	                    return "&";	  
+	                case PUNTERO:
+	                    return "@";	  
+	                default:
+	                    throw new IllegalArgumentException("Invalid operator");
+	            }
+	        }
+	    }
+	    private final Operadores op;
+	    private final Expresion izquierda;
+	    private final Expresion derecha;
+
+	    public OperBin(Operadores op, Expresion izquierda, Expresion derecha) {
+	        this.op = op;
+	        this.izquierda = izquierda;
+	        this.derecha = derecha;
+	    }
+
+	    public Expresion getLeft() {
+	        return izquierda;
+	    }
+
+	    public Expresion getRight() {
+	        return derecha;
+	    }
+
+	    public Operadores getOp() {
+	        return op;
+	    }
+	    
+	    public String toString() {
+	    	return izquierda + op.toString() + derecha;
+	    }
+
 }
