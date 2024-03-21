fb842bc1acf7586abe93f823ed14237e14f96e9a
Mario <mcalvarr@ucm.es>
2024-03-21 17:36:31 +0100

    Fix vector type

--- jajaLANG/src/test/sintactico/Fibonacci.java
+++ jajaLANG/src/test/sintactico/Fibonacci.java
@@ -13,7 +13,20 @@ import main.lexico.AnalizadorLexicoJaja;
 import main.sintactico.AnalizadorSintacticoJaja;
 
 class Fibonacci {
-	private static String result = "";
+	private static String result = "diver fibonacci(r: ent) -> ent {\n"
+			+ "si c == 0 {\n"
+			+ "devuelve 1;\n"
+			+ "} \n"
+			+ "si c == 1 {\n"
+			+ "devuelve 1;\n"
+			+ "} \n"
+			+ "devuelve fibonacci(c - 1) + fibonacci(c - 2);\n"
+			+ "}\n"
+			+ "diver tronco() {\n"
+			+ "para i = 0 -> 10 {\n"
+			+ "escribirEntero(fibonacci(i))\n"
+			+ "}\n"
+			+ "}\n";
 			
 	@Test
 	void test() {
