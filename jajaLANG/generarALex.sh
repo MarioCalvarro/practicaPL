11858841fab10dc8fd425723f7d61601779e38df
Mario <mcalvarr@ucm.es>
2024-04-04 13:34:21 +0200

    Fix script ALex

--- jajaLANG/generarALex.sh
+++ jajaLANG/generarALex.sh
@@ -3,5 +3,5 @@
 #Analizador léxico
 java -cp libraries/jflex.jar jflex.Main src/main/java/lexico/ALexJaja.l
 echo Creado AnalizadorLexicoJaja
-rm src/main/lexico/AnalizadorLexicoJaja.java~
+rm src/main/java/lexico/AnalizadorLexicoJaja.java~
 echo Eliminada la copia de seguridad
