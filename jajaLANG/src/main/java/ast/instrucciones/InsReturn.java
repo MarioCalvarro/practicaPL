4d1437a69fdac4f00ba285ea4457184a753beb30
beñat P. de Arenaza <benperez@ucm.es>
2024-04-16 19:00:57 +0200

    .

--- jajaLANG/src/main/java/ast/instrucciones/InsReturn.java
+++ jajaLANG/src/main/java/ast/instrucciones/InsReturn.java
@@ -3,8 +3,11 @@ package main.java.ast.instrucciones;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.ast.Contexto;
 import main.java.ast.Nodo;
 import main.java.ast.expresiones.Expresion;
+import main.java.ast.tipos.Tipo;
+import main.java.ast.tipos.TipoBinario;
 
 public class InsReturn extends Instruccion {
     private final Expresion expr;
@@ -26,4 +29,15 @@ public class InsReturn extends Instruccion {
 		}
 		return lista;
 	}
+	
+	@Override
+    public void typecheck() {
+        super.typecheck();
+
+        Tipo tipoIndice = condicion.tipo();
+        if (!tipoIndice.equals(TipoBinario.instancia())) {
+        	//TODO : Cambiar error
+            throw new RuntimeException();        
+        }
+    }
 }
