f41d27a297fdc53884896558d3299211856acf07
Mario <mcalvarr@ucm.es>
2024-04-16 16:14:03 +0200

    Bind de identificador

--- jajaLANG/src/main/java/ast/expresiones/Identificador.java
+++ jajaLANG/src/main/java/ast/expresiones/Identificador.java
@@ -3,11 +3,14 @@ package main.java.ast.expresiones;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.ast.Contexto;
 import main.java.ast.Nodo;
+import main.java.ast.declaraciones.Declaracion;
 
 public class Identificador extends Expresion {
     private final String lib;
     private final String id;
+    private Declaracion dec = null;
 
     public Identificador(String id, String lib) {
         this.id = id;
@@ -29,6 +32,19 @@ public class Identificador extends Expresion {
     public boolean externo() {
         //Es externo ← viene de librería
         return lib != null;
+    }
+
+    @Override
+    public void bind(Contexto ctx) {
+        super.bind(ctx);
+        
+        dec = ctx.get(this);
+        if (dec == null) {
+            //TODO: Cambiar error
+            throw new RuntimeException();
+        }
+
+        this.tipo = dec.tipo();
     }
 
     @Override
