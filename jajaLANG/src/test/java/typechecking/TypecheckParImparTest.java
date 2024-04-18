63b33ddb25cf233e921b0d58c41d2ab9ac6f6b06
beñat P. de Arenaza <benperez@ucm.es>
2024-04-18 13:41:10 +0200

    Creacion Test Typecheck de programas completos

---
+++ jajaLANG/src/test/java/typechecking/TypecheckParImparTest.java
@@ -0,0 +1,40 @@
+/*package test.java.typechecking;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.ast.Programa;
+import main.java.lexico.AnalizadorLexicoJaja;
+import main.java.sintactico.AnalizadorSintacticoJaja;
+
+public class TypecheckParImparTest {
+	@Test
+    void test() {
+        //Programa que importa una librería y llama a una de sus funciones
+        Reader input = null;
+        try {
+            input = new InputStreamReader(new FileInputStream("src/test/resources/bindingTypeChecking/parImpar.jaja"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        AnalizadorLexicoJaja al = new AnalizadorLexicoJaja(input);
+        AnalizadorSintacticoJaja as = new AnalizadorSintacticoJaja(al);
+        Programa pr = null;
+        try {
+            pr = (Programa) as.parse().value;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        //Solo queremos comprobar que no tire excepción
+        pr.bind();
+        pr.typecheck();
+    }
+}*/
+
+
