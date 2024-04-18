757bfe5a67786a4701412e15e16e535f1448bfd2
Mario <mcalvarr@ucm.es>
2024-04-18 13:48:40 +0200

    Creados test bind para programas 'reales'

---
+++ jajaLANG/src/test/java/binding/BindFibonacciTest.java
@@ -0,0 +1,36 @@
+// package test.java.binding;
+//
+// import main.java.ast.Programa;
+// import main.java.lexico.AnalizadorLexicoJaja;
+// import main.java.sintactico.AnalizadorSintacticoJaja;
+// import org.junit.jupiter.api.Test;
+//
+// import java.io.FileInputStream;
+// import java.io.FileNotFoundException;
+// import java.io.InputStreamReader;
+// import java.io.Reader;
+//
+// class BindFibonacciTest {
+//     @Test
+//     void test() {
+//         //Programa que importa una librería y llama a una de sus funciones
+//         Reader input = null;
+//         try {
+//             input = new InputStreamReader(new FileInputStream("src/test/resources/fibonacci.jaja"));
+//         } catch (FileNotFoundException e) {
+//             e.printStackTrace();
+//             System.exit(1);
+//         }
+//         AnalizadorLexicoJaja al = new AnalizadorLexicoJaja(input);
+//         AnalizadorSintacticoJaja as = new AnalizadorSintacticoJaja(al);
+//         Programa pr = null;
+//         try {
+//             pr = (Programa) as.parse().value;
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//             System.exit(1);
+//         }
+//         //Solo queremos comprobar que no tire excepción
+//         pr.bind();
+//     }
+// }
