83ff76c936ad167b2115152b0309a883b42ed949
beñat P. de Arenaza <benperez@ucm.es>
2024-04-18 16:33:43 +0200

    Cambiar Tests a forma estandar

--- jajaLANG/src/test/java/binding/BindAliasTest.java
+++ jajaLANG/src/test/java/binding/BindAliasTest.java
@@ -1,10 +1,6 @@
 package test.java.binding;
 
-import main.java.ast.Ambito;
-import main.java.ast.Contexto;
 import main.java.ast.Programa;
-import main.java.ast.declaraciones.DeclaracionAlias;
-import main.java.ast.declaraciones.DeclaracionVar;
 import main.java.lexico.AnalizadorLexicoJaja;
 import main.java.sintactico.AnalizadorSintacticoJaja;
 import org.junit.jupiter.api.Test;
@@ -14,19 +10,11 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class BindAliasTest {
-    private static final String result = "Contexto:\n"
-            + "Ámbito{a=int a = 2, int=INCOGNITO int = ent}\n";
-
     @Test
     void test() {
-        // 1) Crear el AST
-        // 2) Crear un alias y despues usarlo para declarar otra variable.
-        // 3) Imprimir el contexto
+        //Programa que importa una librería y llama a una de sus funciones
         Reader input = null;
-        String test1 = "";
         try {
             input = new InputStreamReader(new FileInputStream("src/test/resources/bindingTypeChecking/alias.jaja"));
         } catch (FileNotFoundException e) {
@@ -42,28 +30,7 @@ class BindAliasTest {
             e.printStackTrace();
             System.exit(1);
         }
-        DeclaracionAlias alias = null;
-        try {
-            alias = (DeclaracionAlias) pr.getAstHijos().get(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        DeclaracionVar var = null;
-        try {
-            var = (DeclaracionVar) pr.getAstHijos().get(1);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        Contexto ctx = new Contexto(null, new Ambito());
-
-        for (var hijo : pr.getAstHijos()) {
-            hijo.bind(ctx);
-        }
-
-        System.out.println(ctx);
-        test1 = ctx.toString();
-        assertEquals(test1, result);
+        //Solo queremos comprobar que no tire excepción
+        pr.bind();
     }
 }
--- jajaLANG/src/test/java/binding/BindWhileTest.java
+++ jajaLANG/src/test/java/binding/BindWhileTest.java
@@ -1,10 +1,6 @@
 package test.java.binding;
 
-import main.java.ast.Ambito;
-import main.java.ast.Contexto;
 import main.java.ast.Programa;
-import main.java.ast.declaraciones.DeclaracionFun;
-import main.java.ast.instrucciones.InsBucleWhile;
 import main.java.lexico.AnalizadorLexicoJaja;
 import main.java.sintactico.AnalizadorSintacticoJaja;
 import org.junit.jupiter.api.Test;
@@ -14,22 +10,11 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class BindWhileTest {
-    private static final String result = "Contexto:\n"
-            + "Ámbito{v1=ent v1, cond=bin cond = fake, v3=ent v3}\n"
-            + "Contexto:\n"
-            + "Ámbito{vW1=ent vW1}\n";
-
     @Test
     void test() {
-        // 1) Crear el AST
-        // 2) Crear dos contextos. Uno con solo el while y otro con la
-        // función. Con esto comprobamos que el total no tiene lo del while
-        // 3) Imprimir los contextos
+        //Programa que importa una librería y llama a una de sus funciones
         Reader input = null;
-        String test1 = "";
         try {
             input = new InputStreamReader(new FileInputStream("src/test/resources/bindingTypeChecking/while.jaja"));
         } catch (FileNotFoundException e) {
@@ -45,38 +30,7 @@ class BindWhileTest {
             e.printStackTrace();
             System.exit(1);
         }
-        DeclaracionFun main = null;
-        try {
-            //Se supone que el único hijo es la declaración del main
-            main = (DeclaracionFun) pr.getAstHijos().get(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        InsBucleWhile bucle = null;
-        try {
-            //Se supone que la segunda instrucción (no hay parámetros es el
-            //bucle)
-            bucle = (InsBucleWhile) main.getAstHijos().get(2);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        //Programa en el constructor puede ser nulo porque no hay importaciones
-        Contexto ctxMain = new Contexto(null, new Ambito());
-        Contexto ctxBucle = new Contexto(null, new Ambito());
-        // main.bind(ctxMain); No podemos hacer esto porque así al final el
-        // contexto sería nulo
-        for (var hijo : main.getAstHijos()) {
-            hijo.bind(ctxMain);
-        }
-        for (var hijo : bucle.getAstHijos()) {
-            hijo.bind(ctxBucle);
-        }
-        System.out.println(ctxMain);
-        System.out.println(ctxBucle);
-
-        test1 = ctxMain.toString() + ctxBucle.toString();
-        assertEquals(test1, result);
+        //Solo queremos comprobar que no tire excepción
+        pr.bind();
     }
 }
