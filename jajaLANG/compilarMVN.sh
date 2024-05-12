b5692176b2ebb8a4ba999969f5dfcbdf50c9fee7
Mario <mcalvarr@ucm.es>
2024-05-12 18:16:58 +0200

    Fix compilarMVN

--- jajaLANG/compilarMVN.sh
+++ jajaLANG/compilarMVN.sh
@@ -8,4 +8,4 @@ fi
 
 # Execute the script with the argument inserted
 mvn clean compile exec:java -Dexec.args="compilar src/test/resources/$1.jaja"
-wat2wasm src/test/resources/bin/$1.wat -o src/test/resources/bin/$1.wasm
+wat2wasm $1.wat -o $1.wasm
